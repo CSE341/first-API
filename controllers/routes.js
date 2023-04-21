3d43689a2faffe6248cc87de5a1f7bf2c86922c4
jym126 <jym126@hotmail.com>
2023-04-21 16:36:27 +0200

    handleErrors

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,3 +1,4 @@
+const { send } = require('process');
 const myContacts = require('../models/contacts');
 
 //home route
@@ -7,48 +8,61 @@ const home = (req, res) => {
 
 const oneContact = async(req, res) => {
     const id = req.params['id'];
-    const myContact = await myContacts.findOne({_id : id});
 
-    res.send(
-        `<table>
-        <tr>
-        <th>Contact information</th>
-        </tr>
-        <tr>
-        <td>${myContact.firstName}</td>
-        <td>${myContact.lastName} -</td>
-        <td>${myContact.email} -</td>
-        <td>${myContact.favoriteColor} -</td>
-        <td>${myContact.birthday}</td>
-        </tr>
-    </table>`
-    )
+    try {
+        
+        const myContact = await myContacts.findOne({_id : id});
+    
+        res.send(
+            `<table>
+            <tr>
+            <th>Contact information</th>
+            </tr>
+            <tr>
+            <td>${myContact.firstName}</td>
+            <td>${myContact.lastName} -</td>
+            <td>${myContact.email} -</td>
+            <td>${myContact.favoriteColor} -</td>
+            <td>${myContact.birthday}</td>
+            </tr>
+        </table>`
+        )
+    } catch (error) {
+        res.send('Ups! something was wrong, please check the route');
+        console.log(error);
+    }
 }
 
 const contacts = async(req, res) => {
 
-    const allContacts = await myContacts.find({});
-
-    let elements = '';
-    const head =
-    `<table>
-    <tr>
-    <th>All contacts information:</th>
-    </tr>
-    </table>`;
-    for (const file of allContacts){
-        elements += 
+    try {
+        
+        const allContacts = await myContacts.find({});
+    
+        let elements = '';
+        const head =
         `<table>
-            <tr>
-            <td>${file.firstName}</td>
-            <td>${file.lastName} -</td>
-            <td>${file.email} -</td>
-            <td>${file.favoriteColor} -</td>
-            <td>${file.birthday}</td>
-            </tr>
-        </table>`
+        <tr>
+        <th>All contacts information:</th>
+        </tr>
+        </table>`;
+        for (const file of allContacts){
+            elements += 
+            `<table>
+                <tr>
+                <td>${file.firstName}</td>
+                <td>${file.lastName} -</td>
+                <td>${file.email} -</td>
+                <td>${file.favoriteColor} -</td>
+                <td>${file.birthday}</td>
+                </tr>
+            </table>`
+        }
+        res.send(head + elements);
+    } catch (error) {
+        res.send('Ups! something was wrong, please check the route');
+        console.log(error);
     }
-    res.send(head + elements);
 }
 
 
